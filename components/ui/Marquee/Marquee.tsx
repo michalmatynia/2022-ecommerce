1076c333aed38f6b685e77d6b1d0fc3b47be4e28
Michal <mmatynia@gmail.com>
2022-12-24 22:32:46 +0100

    po przerwie

--- components/ui/Marquee/Marquee.tsx
+++ components/ui/Marquee/Marquee.tsx
@@ -1,13 +1,19 @@
 import { ReactNode, FC } from "react";
 import styles from "./Marquee.module.css";
+import cx from "classnames";
 
 interface Props {
   children: ReactNode[];
+  variant?: "primary" | "secondary";
 }
 
-export const Marquee: FC<Props> = ({ children }) => {
+export const Marquee: FC<Props> = ({ children, variant = "primary" }) => {
+  const rootClassName = cx(styles.root, {
+    [styles.secondary]: variant === "secondary",
+  });
+
   return (
-    <div className={styles.root}>
+    <div className={rootClassName}>
       <div className={styles.container}>{children}</div>
     </div>
   );
