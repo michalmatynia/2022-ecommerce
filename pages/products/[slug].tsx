828ba10db7ab1410a3c3e1b05bef0dafd5bd48cc
Michal <mmatynia@gmail.com>
2023-01-08 22:13:58 +0100

    some new

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -1,45 +1,45 @@
-import { Layout } from "@components/common";
-import { getConfig } from "@framework/api/config";
-import { getAllProductsPaths, getProduct } from "@framework/product";
-import {
-  GetStaticPaths,
-  GetStaticPropsContext,
-  InferGetStaticPropsType,
-} from "next";
+// import { Layout } from "@components/common";
+// import { getConfig } from "@framework/api/config";
+// import { getAllProductsPaths, getProduct } from "@framework/product";
+// import {
+//   GetStaticPaths,
+//   GetStaticPropsContext,
+//   InferGetStaticPropsType,
+// } from "next";
+// import { ProductView } from "@components/product";
+// // Server
+// // fetch all product slugs
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const config = getConfig();
 
-// Server
-// fetch all product slugs
-export const getStaticPaths: GetStaticPaths = async () => {
-  const config = getConfig();
+//   const { products } = await getAllProductsPaths(config);
+//   return {
+//     paths: products.map((p) => ({ params: { slug: p.slug } })),
+//     fallback: false,
+//   };
+// };
 
-  const { products } = await getAllProductsPaths(config);
-  return {
-    paths: products.map((p) => ({ params: { slug: p.slug } })),
-    fallback: false,
-  };
-};
+// // provide product specific data to the page
+// export const getStaticProps = async ({
+//   params,
+// }: GetStaticPropsContext<{ slug: string }>) => {
+//   const config = getConfig();
+//   const { product } = await getProduct({
+//     config,
+//     variables: { slug: params?.slug },
+//   });
 
-// provide product specific data to the page
-export const getStaticProps = async ({
-  params,
-}: GetStaticPropsContext<{ slug: string }>) => {
-  const config = getConfig();
-  const { product } = await getProduct({
-    config,
-    variables: { slug: params?.slug },
-  });
+//   return {
+//     props: {
+//       product,
+//     },
+//   };
+// };
 
-  return {
-    props: {
-      product,
-    },
-  };
-};
+// export default function ProductSlug({
+//   product,
+// }: InferGetStaticPropsType<typeof getStaticProps>) {
+//   return <>{product && <ProductView product={product} />}</>;
+// }
 
-export default function ProductSlug({
-  product,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <div>{JSON.stringify(product)}</div>;
-}
-
-ProductSlug.Layout = Layout;
+// ProductSlug.Layout = Layout;
