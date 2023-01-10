b52afeb1312492920d424e3cb6fcc03bd85c0025
Michal <mmatynia@gmail.com>
2023-01-10 03:52:31 +0100

    notworking1

---
+++ components/product/Swatch/Swatch.tsx
@@ -0,0 +1,43 @@
+import React, { FC } from "react";
+import s from "./Swatch.module.css";
+import { Check } from "@components/icons";
+import cn from "classnames";
+import { isDark } from "@lib/color";
+
+interface Props {
+  color?: string;
+  label?: string;
+  active: boolean;
+  variant?: "size" | "color" | string;
+  onClick: () => void;
+}
+
+const Swatch: FC<Props> = ({ color, label, variant, active, ...rest }) => {
+  label = label?.toLowerCase();
+  variant = variant?.toLowerCase();
+
+  const rootClassName = cn(s.root, {
+    [s.active]: active,
+    [s.color]: color,
+    [s.size]: variant === "size",
+    [s.dark]: color && isDark(color),
+  });
+
+  return (
+    <button
+      className={rootClassName}
+      style={color ? { backgroundColor: color } : {}}
+      {...rest}
+    >
+      {variant === "color" && active && (
+        <span>
+          <Check />
+        </span>
+      )}
+
+      {variant === "size" ? label : null}
+    </button>
+  );
+};
+
+export default Swatch;
